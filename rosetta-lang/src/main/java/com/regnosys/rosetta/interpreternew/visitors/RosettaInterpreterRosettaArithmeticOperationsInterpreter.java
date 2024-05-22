ec98b166a42425f42403eb8329e97383f4c2d24f
Bogdan Damian <r.b.damian@student.tudelft.nl>
2024-05-23 00:09:53 +0200

    Fixed all of the checkstyle errors

--- rosetta-lang/src/main/java/com/regnosys/rosetta/interpreternew/visitors/RosettaInterpreterRosettaArithmeticOperationsInterpreter.java
+++ rosetta-lang/src/main/java/com/regnosys/rosetta/interpreternew/visitors/RosettaInterpreterRosettaArithmeticOperationsInterpreter.java
@@ -13,12 +13,19 @@ import com.regnosys.rosetta.rosetta.expression.RosettaExpression;
 import com.regnosys.rosetta.rosetta.interpreter.RosettaInterpreterValue;
 import com.rosetta.model.lib.RosettaNumber;
 
-public class RosettaInterpreterRosettaArithmeticOperationsInterpreter extends RosettaInterpreterConcreteInterpreter{
-
+public class RosettaInterpreterRosettaArithmeticOperationsInterpreter 
+					extends RosettaInterpreterConcreteInterpreter {
+	/**
+	 * Interprets an arithmetic operation, evaluating the operation between the two terms.
+	 *
+	 * @param expr The ArithmeticOperation expression to interpret
+	 * @return If no errors are encountered, a RosettaInterpreterNumberValue or
+	 * 		   RosettaInterpreterStringValue representing
+	 * 		   the result of the arithmetic/concatenation operation.
+	 * 		   If errors are encountered, a RosettaInterpreterErrorValue representing
+     *         the error.
+	 */
 	public RosettaInterpreterValue interp(ArithmeticOperation expr) {
-
-		RosettaNumber leftNumber = new RosettaNumber("0");
-		RosettaNumber rightNumber = new RosettaNumber("0");
 		
 		String leftString = null;
 		String rightString = null;
@@ -28,46 +35,65 @@ public class RosettaInterpreterRosettaArithmeticOperationsInterpreter extends Ro
 		RosettaInterpreterValue leftInterpreted = left.accept(visitor);
 		RosettaInterpreterValue rightInterpreted = right.accept(visitor); 
 		
-		if (!(leftInterpreted instanceof RosettaInterpreterNumberValue || 
-				leftInterpreted instanceof RosettaInterpreterStringValue ||
-				leftInterpreted instanceof RosettaInterpreterIntegerValue) ||
-				!(rightInterpreted instanceof RosettaInterpreterNumberValue || 
-						rightInterpreted instanceof RosettaInterpreterStringValue ||
-						rightInterpreted instanceof RosettaInterpreterIntegerValue)) {
+		if (!(leftInterpreted instanceof RosettaInterpreterNumberValue 
+				|| leftInterpreted instanceof RosettaInterpreterStringValue 
+				|| leftInterpreted instanceof RosettaInterpreterIntegerValue) 
+				|| !(rightInterpreted instanceof RosettaInterpreterNumberValue 
+				|| rightInterpreted instanceof RosettaInterpreterStringValue 
+				|| rightInterpreted instanceof RosettaInterpreterIntegerValue)) {
 			
-				// Check for errors in the left or right side of the binary operation
-				RosettaInterpreterErrorValue leftErrors = 
-						checkForErrors(leftInterpreted, "Leftside");
-				RosettaInterpreterErrorValue rightErrors = 
-						checkForErrors(rightInterpreted, "Rightside");
-				return RosettaInterpreterErrorValue.merge(List.of(leftErrors, rightErrors));
+			// Check for errors in the left or right side of the binary operation
+			RosettaInterpreterErrorValue leftErrors = 
+					checkForErrors(leftInterpreted, "Leftside");
+			RosettaInterpreterErrorValue rightErrors = 
+					checkForErrors(rightInterpreted, "Rightside");
+			return RosettaInterpreterErrorValue.merge(List.of(leftErrors, rightErrors));
 		}
 		
+		RosettaNumber leftNumber;
+		RosettaNumber rightNumber;
 		
 		if (leftInterpreted instanceof RosettaInterpreterStringValue) {
 			leftString = ((RosettaInterpreterStringValue) leftInterpreted).getValue();
 			rightString = ((RosettaInterpreterStringValue) rightInterpreted).getValue();
-			if(expr.getOperator().equals("+"))
+			if (expr.getOperator().equals("+")) {
 				return new RosettaInterpreterStringValue(leftString + rightString);
-			else return new RosettaInterpreterErrorValue(
+				}
+			else {
+				return new RosettaInterpreterErrorValue(
 					new RosettaInterpreterError(
-							"The terms are strings but the operation is not concatenation: not implemented"));
+				"The terms are strings but the operation "
+				+ "is not concatenation: not implemented"));
+			}
 		}
 			
-		if (leftInterpreted instanceof RosettaInterpreterNumberValue)
+		if (leftInterpreted instanceof RosettaInterpreterNumberValue) {
 			leftNumber = ((RosettaInterpreterNumberValue) leftInterpreted).getValue();
-			else leftNumber = RosettaNumber.valueOf(((RosettaInterpreterIntegerValue) leftInterpreted).getValue());
-		if (rightInterpreted instanceof RosettaInterpreterNumberValue)
+		}
+			else {
+				leftNumber = RosettaNumber
+					.valueOf(((RosettaInterpreterIntegerValue) leftInterpreted)
+					.getValue());
+				}
+		if (rightInterpreted instanceof RosettaInterpreterNumberValue) {
 			rightNumber = ((RosettaInterpreterNumberValue) rightInterpreted).getValue();
-			else rightNumber = RosettaNumber.valueOf(((RosettaInterpreterIntegerValue) rightInterpreted).getValue());
-		if(expr.getOperator().equals("+")) {
-			return new RosettaInterpreterNumberValue((leftNumber.add(rightNumber)).bigDecimalValue());
-		} else if(expr.getOperator().equals("-")) {
-			return new RosettaInterpreterNumberValue((leftNumber.subtract(rightNumber)).bigDecimalValue());
-		} else if(expr.getOperator().equals("*")) {
-			return new RosettaInterpreterNumberValue((leftNumber.multiply(rightNumber)).bigDecimalValue());
 		} else {
-			return new RosettaInterpreterNumberValue((leftNumber.divide(rightNumber)).bigDecimalValue());
+				rightNumber = RosettaNumber
+					.valueOf(((RosettaInterpreterIntegerValue) rightInterpreted)
+					.getValue());
+		}
+		if (expr.getOperator().equals("+")) {
+			return new RosettaInterpreterNumberValue((leftNumber
+					.add(rightNumber)).bigDecimalValue());
+		} else if (expr.getOperator().equals("-")) {
+			return new RosettaInterpreterNumberValue((leftNumber
+					.subtract(rightNumber)).bigDecimalValue());
+		} else if (expr.getOperator().equals("*")) {
+			return new RosettaInterpreterNumberValue((leftNumber
+					.multiply(rightNumber)).bigDecimalValue());
+		} else {
+			return new RosettaInterpreterNumberValue((leftNumber
+					.divide(rightNumber)).bigDecimalValue());
 		}
 	}
 	
@@ -85,12 +111,13 @@ public class RosettaInterpreterRosettaArithmeticOperationsInterpreter extends Ro
 	 */
 	private RosettaInterpreterErrorValue checkForErrors(
 			RosettaInterpreterValue interpretedValue, String side) {
-		if (interpretedValue instanceof RosettaInterpreterNumberValue || 
-				interpretedValue instanceof RosettaInterpreterStringValue ||
-				interpretedValue instanceof RosettaInterpreterIntegerValue)
+		if  (interpretedValue instanceof RosettaInterpreterNumberValue 
+				|| interpretedValue instanceof RosettaInterpreterStringValue 
+				|| interpretedValue instanceof RosettaInterpreterIntegerValue) {
 			// If the value satisfies the type conditions, we return an empty 
 			// error value so that the merger has two error values to merge
 			return new RosettaInterpreterErrorValue();
+		}
 		
 		else if (RosettaInterpreterErrorValue.errorsExist(interpretedValue)) {
 			// The interpreted value was an error so we propagate it
@@ -100,8 +127,8 @@ public class RosettaInterpreterRosettaArithmeticOperationsInterpreter extends Ro
 			// but something other than a string or number
 			return new RosettaInterpreterErrorValue(
 					new RosettaInterpreterError(
-							"Arithmetic Operation: " + side + 
-							" is not of type Number/String"));
+							"Arithmetic Operation: " + side 
+							+ " is not of type Number/String"));
 		}
 	}
 }
