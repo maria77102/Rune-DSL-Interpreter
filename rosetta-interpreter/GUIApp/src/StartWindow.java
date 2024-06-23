a4ab264c77f0e9a361f5b99756ae0078277ed332
Antonio <a.f.lupu@student.tudelft.nl>
2024-06-23 21:33:12 +0200

    Added drop-down for rune code example

--- rosetta-interpreter/GUIApp/src/StartWindow.java
+++ rosetta-interpreter/GUIApp/src/StartWindow.java
@@ -10,6 +10,8 @@ import javax.swing.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Font;
+import java.awt.Color;
 
 public class StartWindow {
 
@@ -17,6 +19,9 @@ public class StartWindow {
 	ExpressionParser parser = injector.getInstance(ExpressionParser.class);
 	ModelHelper modelHelper = injector.getInstance(ModelHelper.class);
 	RosettaInterpreterNew interpreter = injector.getInstance(RosettaInterpreterNew.class); 
+	
+	
+	String example_1 = 
 	
     private JFrame frame;
 
@@ -40,36 +45,63 @@ public class StartWindow {
      */
     private void initialize() {
         frame = new JFrame();
-        frame.setBounds(100, 100, 1180, 653);
+        frame.getContentPane().setFont(new Font("Calibri", Font.PLAIN, 14));
+        frame.setBounds(100, 100, 1363, 772);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(null);
 
         // Scroll pane for code text area
         JScrollPane codeScrollPane = new JScrollPane();
-        codeScrollPane.setBounds(10, 21, 427, 564);
+        codeScrollPane.setBounds(10, 131, 507, 548);
         frame.getContentPane().add(codeScrollPane);
 
         // Text area for code input
         JTextArea codeTextArea = new JTextArea();
         codeTextArea.setRows(31);
-        codeScrollPane.setColumnHeaderView(codeTextArea);
+        codeScrollPane.setViewportView(codeTextArea);
 
         // Button to interpret code
         JButton interpretButton = new JButton("Interpret");
-        interpretButton.setBounds(469, 273, 172, 51);
+        interpretButton.setBackground(new Color(240, 240, 240));
+        interpretButton.setFont(new Font("Calibri", Font.PLAIN, 21));
+        interpretButton.setBounds(540, 351, 172, 51);
         frame.getContentPane().add(interpretButton);
 
         // Scroll pane for result text area
         JScrollPane resultScrollPane = new JScrollPane();
-        resultScrollPane.setBounds(670, 240, 406, 114);
+        resultScrollPane.setBounds(742, 285, 580, 168);
         frame.getContentPane().add(resultScrollPane);
 
         // Text area for displaying results
         JTextArea resultTextArea = new JTextArea();
-        resultTextArea.setRows(6);
+        resultTextArea.setRows(9);
         resultTextArea.setColumns(50);
         resultTextArea.setEditable(false); // Make it read-only
-        resultScrollPane.setColumnHeaderView(resultTextArea);
+        resultScrollPane.setViewportView(resultTextArea);
+        
+        JComboBox<String> tests = new JComboBox<String>();
+        tests.setFont(new Font("Calibri", Font.PLAIN, 14));
+        tests.setEditable(true);
+        tests.setToolTipText("");
+        tests.setBounds(10, 91, 172, 28);
+        frame.getContentPane().add(tests);
+        
+        tests.setSelectedItem("Select Rune Example");
+        
+        JButton exampleButton = new JButton("Get Code");
+        exampleButton.setBackground(new Color(255, 255, 255));
+        exampleButton.setFont(new Font("Calibri", Font.PLAIN, 14));
+        exampleButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		String chosenExample = tests.getSelectedItem().toString();
+        		
+        	}
+        });
+        exampleButton.setBounds(221, 93, 125, 26);
+        frame.getContentPane().add(exampleButton);
+        tests.addItem("Example 1");
+        tests.addItem("Example 2");
+        tests.addItem("Example 3");
 
         // Action listener for interpret button
         interpretButton.addActionListener(new ActionListener() {
